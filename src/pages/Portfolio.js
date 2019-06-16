49cbb9fcef9e687cbd9e334b0d3ec57666e7340a
Rafael Melo <rsmelo_@hotmail.com>
2019-06-16 08:13:09 -0300

    prob done

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import Profile from '../components/Profile/Profile.js';
-import Timeline from '../components/Timeline/Timeline.js';
+import React from "react";
+import Profile from "../components/Profile/Profile.js";
+import Timeline from "../components/Timeline/Timeline.js";
 
-class Portfolio extends Component {
-    render(){
-        return(
-            <div>
-                <Profile/>
-			    <Timeline/>
-            </div>
-        )
-    }
+export default function Portfolio() {
+  return (
+    <div>
+      <Profile />
+      <Timeline />
+    </div>
+  );
 }
-
-export default Portfolio;
